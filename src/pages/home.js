6b967cfd8d0c2a7cf4b85e1de809c985ff5ea219
Cla2 <clarencedave990@gmail.com>
2018-11-28 08:11:36 +0800

    init commit

--- src/pages/home.js
+++ src/pages/home.js
@@ -10,16 +10,17 @@ import Layout from '../components/layout'
 const homePage = () => (
   <Layout>
           <div class = "media"> 
-          
+          <h1>Find Us On:</h1>
+            
           <a  href= "facebook.com"> <img src= {facebook} alt="fb" class = "fb"/></a>
           <a  href= "linkedin.com"> <img src= {linkedin} alt="in"class = "in"/></a>
           <a  href= "twitter.com"> <img src= {twit} alt="twit"class = "twit"/></a>
           <a  href= "gmail.com"> <img src= {mail} alt="mail"class = "mail"/></a>
           <div class="hero-image">
           <div class="hero-text">
-         
-            <h1>Find Us</h1>
-            <button>Learn More!</button>
+         <br/>
+         <br/>
+          <button>Learn More!</button>
           </div>
           </div>
           </div>
